1b45de7556595ab6938e1f6cd26db1f2fa98d37b
Maciej Weber <maciejwebber@gmail.com>
2022-03-29 19:57:58 +0200

    base64 decoder

--- digits/api/serializers.py
+++ digits/api/serializers.py
@@ -11,9 +11,13 @@ class Base64ImageField(serializers.ImageField):
         _format, str_img = data.split(';base64')
         decoded_file = base64.b64decode(str_img)
         fname = f"{str(uuid.uuid4())[:10]}.png"
+        data = ContentFile(decoded_file, name=fname)
+        return super().to_internal_value(data)
 
 
 class DigitSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
+
     class Meta:
         model = Digit
         fields = ('id', 'image', 'result')
